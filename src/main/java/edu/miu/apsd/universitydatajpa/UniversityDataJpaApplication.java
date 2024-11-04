0c661d8295d6a803412d9065309b4e783a6a4892
sanjeevthapamiu <sanjeev.thapa@miu.edu>
2024-11-04 17:12:52 -0600

    Project Initialization

---
+++ src/main/java/edu/miu/apsd/universitydatajpa/UniversityDataJpaApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.apsd.universitydatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityDataJpaApplication.class, args);
+    }
+
+}
