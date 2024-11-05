42b3fb130f4748d807591c2e34ddfac15032521f
sanjeevthapamiu <sanjeev.thapa@miu.edu>
2024-11-04 21:51:54 -0600

    Add Find and Filter methods in Repository

--- src/main/java/edu/miu/apsd/universitydatajpa/repository/CourseRepository.java
+++ src/main/java/edu/miu/apsd/universitydatajpa/repository/CourseRepository.java
@@ -4,6 +4,11 @@ import edu.miu.apsd.universitydatajpa.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByDepartmentNameOrderByTitle(String departmentName);
+
 }
--- src/main/java/edu/miu/apsd/universitydatajpa/repository/DepartmentRepository.java
+++ src/main/java/edu/miu/apsd/universitydatajpa/repository/DepartmentRepository.java
@@ -4,6 +4,11 @@ import edu.miu.apsd.universitydatajpa.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    List<Department> findAllByOrderByName();
+
 }
